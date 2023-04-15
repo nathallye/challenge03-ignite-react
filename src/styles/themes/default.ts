6fa56c663c4014cd91bc5db88b432db7085f401b
Nathallye Bacelar <nathallyet@gmail.com>
2023-04-15 01:49:57 -0300

    Adjustments fonts and themes

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,22 +1,15 @@
 export const defaultTheme = {
   white: "#FFF",
 
-  "gray-100": "#E1E1E6",
-  "gray-300": "#C4C4CC",
-  "gray-400": "#8D8D99",
-  "gray-500": "#7C7C8A",
-  "gray-600": "#323238",
-  "gray-700": "#29292E",
-  "gray-800": "#202024",
-  "gray-900": "#121214",
-
-  "green-300": "#00B37E",
-  "green-500": "#00875F",
-  "green-700": "#015F43",
-
-  "red-300": "#F75A68",
-  "red-500": "#AB222E",
-  "red-700": "#7A1921",
-
-  "yellow-500": "#FBA94C"
+  "blue-100": "#3294F8",
+  "base-title": "#E7EDF4",
+  "base-subtitle": "#C4D4E3",
+  "base-text": "#AFC2D4",
+  "base-span": "#7B96B2",
+  "base-label": "#3A536B",
+  "base-border": "#1C2F41",
+  "base-post": "#112131",
+  "base-profile": "#0B1B2B",
+  "base-background": "#071422",
+  "base-input": "#040F1A"
 } as const;

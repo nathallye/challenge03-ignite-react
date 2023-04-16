a7c1221f9cf0027fcfbafd5e0e8ffab2d2036bc6
Nathallye Bacelar <nathallyet@gmail.com>
2023-04-15 22:51:40 -0300

    Blog page

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import { ThemeProvider } from "styled-components";
 
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
-import { Header } from "./components/Header/Header";
+
+import { Blog } from "./pages/Blog";
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Header />
-
-      <h1>Hello!</h1>
+      
+      <Blog />
     </ThemeProvider>
   )
 };
